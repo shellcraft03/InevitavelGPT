ff95e6363370113e066da81cf8de22d32b1d29c2
Elias Barbosa <contato@eliasbarbosa.com>
2026-05-01 13:45:10 -0300

    fix: cleanup Turnstile widget on unmount and suppress scroll-behavior warning

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="pt-BR">
+    <Html lang="pt-BR" data-scroll-behavior="smooth">
       <Head>
         <link rel="icon" href="/cover.png" />
         <link rel="apple-touch-icon" href="/cover.png" />
